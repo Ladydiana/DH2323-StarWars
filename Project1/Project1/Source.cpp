c68876159c12fa5566915f87bd969f6562262ce8
Ladydiana <diana.culincu@gmail.com>
2022-04-04 21:47:55 +0200

    Draw Romanian flag

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -46,18 +46,39 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void Draw()
-{
+
+void DrawRomanianFlag() {
 
 	for (int y = 0; y < SCREEN_HEIGHT; ++y)
 	{
 
-		for (int x = 0; x < SCREEN_WIDTH; ++x)
+		for (int x = 0; x < SCREEN_WIDTH / 3; ++x)
 		{
-			vec3 color(0, 0, 1);
-			PutPixelSDL(screen, x, y, color);
+			vec3 red(139, 0, 0);
+			PutPixelSDL(screen, x, y, red);
+
+		}
+
+		for (int x = SCREEN_WIDTH / 3; x < 2 * (SCREEN_WIDTH / 3); ++x)
+		{
+			vec3 yellow(1.000, 0.833, 0.224);
+			PutPixelSDL(screen, x, y, yellow);
+		}
+
+		for (int x = 2 * (SCREEN_WIDTH / 3); x < SCREEN_WIDTH; ++x)
+		{
+
+			vec3 blue(0, 0, 1);
+			PutPixelSDL(screen, x, y, blue);
 		}
 	}
+
+}
+
+void Draw()
+{
+	DrawRomanianFlag();
+	
 
 	if (SDL_MUSTLOCK(screen))
 		SDL_UnlockSurface(screen);
